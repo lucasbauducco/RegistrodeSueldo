6525f6626d2f7bc4b954e4dcceaf6505132e58b8
Hunter086 <lucasbauducco@gmail.com>
2021-08-18 15:43:38 -0300

    actualizacion

--- src/Controller/ReciboController.php
+++ src/Controller/ReciboController.php
@@ -13,19 +13,31 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Regex;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 
 use App\Form\CargarRecibosType;
 class ReciboController extends AbstractController
 {
     /**
+     * Carga una lista de recibos a la tabla recibos de la bd y los guarda en una carpeta uploads
      * 
-     * @Route("/cargarRecibo/{anio}/{mes}", name="app_cargar_recibo")
+     * conficurar php.ini server
+     * 
+     * max_file_uploads = 600
+     * memory_limit = 200M
+     * post_max_size = 200M
+     * upload_max_filesize = 600M
+     * post_max_size = 650M
+     * max_execution_time = 500
+     * max_input_time = 500
+     * 
+     * @Route("/cargarRecibo", name="app_cargar_recibo")
      */
-    public function cargarRecibo(Request $request,SluggerInterface $slugger, $anio,$mes){
+    public function cargarRecibo(Request $request,SluggerInterface $slugger){
         $manager= $this->getDoctrine()->getManager();
         $dateTime = new \DateTime();
-        $day = $dateTime->format('d-m-Y');
+        //$day = $dateTime->format('d-m-Y');
 
         $manager=$this->getDoctrine()->getManager();
         
@@ -37,49 +49,100 @@ class ReciboController extends AbstractController
         
         if ($formulario->isSubmitted() && $formulario->isValid()) {
             /** @var UploadedFile $brochureFile */
+                //traigo los archivos cargados del formulario y los almaceno en una variable
                 $archivos = $formulario->get('archivos')->getData();
-                if ($archivos!=null) {
+                $mes=$formulario->get('mes')->getData();
+                $anio=$formulario->get('fecha')->getData();
+                $anio=  $anio->format("Y");
+                
+                try {
                     foreach ($archivos as $archivo) {
-                        try {
-                                
-                                
+                        
+                            
+                                //extraigo el nombre del archivo
                                 $originalFilename = pathinfo($archivo->getClientOriginalName(), PATHINFO_FILENAME);
                                 $usuario= new User();
+                                //busco el usuario por el legajo cargado en el nombre del archivo
                                 $usuario= $manager->getRepository(User::class)->findOneBy(array('legajo' =>  $originalFilename));
-                                //$brochureFile->getClientOriginalName()
-                                // this is needed to safely include the file name as part of the URL
-                                $safeFilename = $slugger->slug($originalFilename);
-                                $newFilename = $safeFilename.'.'.$archivo->guessExtension();
-                                $newFilename= '..\\uploads\\recibos\\'."".$anio.'\\'."".$mes.'\\'.$newFilename;
+                                 //$brochureFile->getClientOriginalName()
+                                        // this is needed to safely include the file name as part of the URL
+                                        //creo la dirreccion url del archivo
+                                        $safeFilename = $slugger->slug($originalFilename);
+                                        $newFilename = $safeFilename.'.'.$archivo->guessExtension();
+                                        $newFilename= '\\uploads\\recibos\\'."".$anio.'\\'."".$mes.'\\'.$newFilename;
+                                        //si tienen correo se les asigna sino se les sea no asignado
+                                if($usuario!=null)
+                                {
+                                       
+                                        //genero un objeto recibo y seteo los parametros por constructor
 
-                                $documento= new Recibo($originalFilename,$usuario->getEmail(),$dateTime, $newFilename, $anio,$mes,"Activo");
+                                        $documento= new Recibo($originalFilename,$usuario->getEmail(),$dateTime, $newFilename, $anio,$mes,"Activo");
+                                        //almaceno el archivo en la carpeta
+                                        $uploads_dir = '\\uploads\\recibos\\'."".$anio.'\\'."".$mes;
+                                        
+                                        $archivo->move(
+                                            $uploads_dir,
+                                            $newFilename
+                                        );
+                                        //guardo en base de datos
+                                        $manager->persist($documento);
+                                        $manager->flush();
+                                    
+                                }else {
+                                    $usuario= null;
+                                    $email= "no tiene";
+                                    //genero un objeto recibo y seteo los parametros por constructor
+
+                                    $documento= new Recibo($originalFilename,$email,$dateTime, $newFilename, $anio,$mes,"Activo");
+                                    //almaceno el archivo en la carpeta
+                                    $uploads_dir = '..\\uploads\\recibos\\'."".$anio.'\\'."".$mes;
+                                    
+                                    $archivo->move(
+                                        $uploads_dir,
+                                        $newFilename
+                                    );
+                                    //guardo en base de datos
+                                    $manager->persist($documento);
+                                    $manager->flush();
+                                    
+                                }
                                 
-                                $uploads_dir = '..\\uploads\\recibos\\'."".$anio.'\\'."".$mes;
-                                $archivo->move(
-                                    $uploads_dir,
-                                    $newFilename
-                                );
-                                $manager->persist($documento);
-                                $manager->flush();
-                        }catch (\Throwable $th) {
-                            $this -> addFlash('error', '¡Error al guardar los archivos!'.$th);
-                            return $this->render('recibo/cargarRecibos.html.twig', [
-                                'formulario' => $formulario->createView(),
-                            ]);
-                        }
+                               
+                                
+                                
                     }
 
-            }
+                }catch (\Throwable $th) {
+                    $this -> addFlash('error', '¡Error al guardar los archivos!'.$th);
+                    return $this->render('recibo/cargarRecibos.html.twig', [
+                        'formulario' => $formulario->createView(),
+                    ]);
+                }
+            $this -> addFlash('succes', '¡Los archivos se cargaron exitosamente!');
             return $this->render('recibo/cargarRecibos.html.twig', [
                 'formulario' => $formulario->createView(),
             ]);
-
-
         }
         return $this->render('recibo/cargarRecibos.html.twig', [
             'formulario' => $formulario->createView(),
         ]);
     }
+
+     /**
+     * @Route("verPDF/{path}", name="verPDF")
+     */
+    public function verPDF(Request $request, $path){
+        $manager=$this->getDoctrine()->getManager();
+
+        
+        
+        return $this->redirect('//recibodesueldo//'.$path);
+
+    }
+  
+
+
+
     /**
      * 
      * @Route("/recibo", name="app_recibo")
@@ -200,7 +263,7 @@ class ReciboController extends AbstractController
                         
                         $configDirectories = array($uploads_dir);
                         //pregunto si existe el archivo en la direccion
-                        $pathArchive='..\\uploads\\recibos\\'."".$i.'\\'."".$mes.'\\'."".$legajo.".pdf";
+                        $pathArchive='uploads\\recibos\\'."".$i.'\\'."".$mes.'\\'."".$legajo.".pdf";
                         
                         if (file_exists('..\\uploads\\recibos\\'."".$i.'\\'."".$mes.'\\'."".$legajo.".pdf")) {
                             //guardo recibos en la tabla
--- src/Form/CargarRecibosType.php
+++ src/Form/CargarRecibosType.php
@@ -11,17 +11,48 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType; 
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CargarRecibosType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+        ->add('archivos', FileType::class, [
+            'attr' => ['class' => 'form-control camposEstandar', 'placeholder' => 'Año Cargado'],
+            'multiple' => true,
+            
+        ])
+        // unmapped means that this field is not associated to any entity property
         
-        
-        ->add('archivos', FileType::class, array('multiple' => true,
-        'attr' => array('class' => 'form-control camposEstandar')))
     
+        // ...
+        ->add('mes', ChoiceType::class, [
+            'attr' => ['class' => 'form-control camposEstandar', 'placeholder' => 'Año Cargado'],
+            'choices'  => [
+                '' => '',
+                'Enero' => 'Enero',
+                'Febrero' => 'Febrero',
+                'Marzo' => 'Marzo',
+                'Abril' => 'Abril',
+                'Mayo' => 'Mayo',
+                'Junio' => 'Junio',
+                'SAC Junio' => 'SAC Junio',
+                'Julio' => 'Julio',
+                'Agosto' => 'Agosto',
+                'Septiembre' => 'Septiembre',
+                'Octubre' => 'Octubre',
+                'Noviembre' => 'Noviembre',
+                'Diciembre' => 'Diciembre',
+                'SAC Diciembre' => 'SAC Diciembre',
+            ],
+        ])
+        ->add('fecha', DateType::class, [
+            'attr' => array('class' => 'form-control camposEstandar', 'placeholder' => 'Año Cargado'),
+            'widget' => 'single_text'
+        ])
+        
         ->add('Guardar',SubmitType::class)
     
         ;
